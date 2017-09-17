392d098c5894123336c873b14eafa916e2b8d9ad
Danilo Shiga <daniloshiga@gmail.com>
2017-09-17 18:10:27 -0300

    Initial commit

---
+++ sentry_api_client/api.py
@@ -0,0 +1,20 @@
+from simple_rest_client.api import API
+
+from sentry_api_client import resource
+
+def get_api_instance(token='', timeout=50):
+    headers = {
+        'Authorization': 'Bearer {}'.format(token),
+        'Content-Type': 'application/json',
+    }
+
+    api_root_url = 'https://sentry.io/api/0/'
+
+    api = API(
+        api_root_url=api_root_url, headers=headers, json_encode_body=True, timeout=timeout
+    )
+
+    api.add_resource(resource_name='project_events', resource_class=resource.ProjectEvents)
+    api.add_resource(resource_name='async_project_events', resource_class=resource.AsyncProjectEvents)
+
+    return api

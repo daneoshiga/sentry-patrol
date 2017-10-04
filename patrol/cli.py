9d327f11f2f8e14223837343932dfe6498096180
Gustavo Siqueira <rsiqueira.gustavo@gmail.com>
2017-10-04 14:35:50 -0300

    Add list issues command

--- patrol/cli.py
+++ patrol/cli.py
@@ -10,11 +10,20 @@ patrol = Patrol(SENTRY_API_TOKEN)
 def cli():
     pass
 
+
 @cli.command(help='Lists project events')
 @click.argument('organization')
 @click.argument('project_name')
 def events(organization, project_name):
     click.secho('events for {}'.format(project_name))
     for event in patrol.events(organization, project_name):
-        import ipdb; ipdb.set_trace()
         click.echo('{e[eventID]}: {e[message]}'.format(e=event))
+
+
+@cli.command(help='Lists project issues')
+@click.argument('organization')
+@click.argument('project_name')
+def issues(organization, project_name):
+    click.secho('issues for {}'.format(project_name))
+    for issue in patrol.issues(organization, project_name):
+        click.echo('{i[id]}: {i[title]}'.format(i=issue))
--- patrol/patrol.py
+++ patrol/patrol.py
@@ -2,6 +2,7 @@ from requests.utils import parse_header_links
 
 from .sentry_api_client.api import get_api_instance
 
+
 class Patrol:
 
     def __init__(self, sentry_api_token, timeout=None):
@@ -18,3 +19,15 @@ class Patrol:
             all_events.extend(events.body)
 
         return all_events
+
+    def issues(self, organization, project):
+        all_issues = []
+        issues = self.api.project_issues.list(organization, project)
+        all_issues.extend(issues.body)
+
+        links = parse_header_links(issues.headers.get('Link'))
+        while links[1]['results'] == 'true':
+            issues = self.api.project_issues.list(organization, project)
+            all_issues.extend(issues.body)
+
+        return all_issues

ca890bc5b54f743734e68e8c84363c4a936bfd92
Gustavo Siqueira <rsiqueira.gustavo@gmail.com>
2017-11-17 13:26:13 -0200

    Add command to fetch and update issues

--- patrol/sentry_api_client/resource.py
+++ patrol/sentry_api_client/resource.py
@@ -3,11 +3,14 @@ from simple_rest_client.resource import Resource
 
 class ProjectEvents(Resource):
     actions = {
+        'fetch': {'method': 'GET', 'url': 'projects/{}/{}/events/{}'},
         'list': {'method': 'GET', 'url': 'projects/{}/{}/events/'}
     }
 
 
 class ProjectIssues(Resource):
     actions = {
-        'list': {'method': 'GET', 'url': 'projects/{}/{}/issues/'}
+        'fetch': {'method': 'GET', 'url': 'issues/{}/'},
+        'list': {'method': 'GET', 'url': 'projects/{}/{}/issues/'},
+        'update': {'method': 'PUT', 'url': 'issues/{}/'},
     }
